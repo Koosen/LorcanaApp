14de7a519e0b58e99cbb872fb81fe3583c4cff7f
Richard Todd <Richardjamestodd@hotmail.co.uk>
2023-11-02 13:22:47 +0000

    cleaned up CSS

--- src/components/SearchResultsList.js
+++ src/components/SearchResultsList.js
@@ -4,7 +4,7 @@ function SearchResultList({ cardSearchResults }){
     return(
         <div className="SearchResultList">
             {cardSearchResults.map((result, id) => {
-                return <div key={id}>{result}</div>
+                return <div key={id} onClick={(e)=>alert(`you clicked ${result}`)}>{result}</div>
             })}
         </div>
     )
