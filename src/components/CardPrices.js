ee95c707495d8be36d9b8ebf1598446098e3923d
Richard Todd <Richardjamestodd@hotmail.co.uk>
2023-11-02 14:57:36 +0000

    fixed css styling

--- src/components/CardPrices.js
+++ src/components/CardPrices.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 function CardPrices(){
+    const { cardName } = useParams();
+
     return (
         <div>
-
+            <p> {cardName} </p>
         </div>
     )
 }
